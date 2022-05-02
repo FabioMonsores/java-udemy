088c15332c90fa900e0064afb39708ec1fc4d1fd
Fábio Monsores <47160837+FabioMonsores@users.noreply.github.com>
2022-05-02 12:48:03 -0300

    Update exercicioVariavelEConstante.java

--- src/exercicios/exercicioVariavelEConstante.java
+++ src/exercicios/exercicioVariavelEConstante.java
@@ -1,25 +1,27 @@
+//Criar um programa que imprima o valor da temperatura que converta graus Farenheit para graus CÃ©lsius
+
 package exercicios;
 
 public class exercicioVariavelEConstante {
 
 	public static void main(String[] args) {
-		// (°F - 32) X 5/9 = °C
-		// Variável para F e C
+		// (Â°F - 32) X 5/9 = Â°C
+		// VariÃ¡vel para F e C
 		// Constante para 32 e 5/9
 				
-		final double ajuste = 32; //32 é um ajuste na fórmula
+		final double ajuste = 32; //32 Ã© um ajuste na fÃ³rmula
 		final double fator = 5.0/9.0; // 5/9 Representa um fator multiplicativo
 		
 		double farenheit = 86;
 		double celsius = (farenheit - ajuste) * fator;
-		System.out.println("O resultado é " + celsius + "°C");
+		System.out.println("O resultado Ã© " + celsius + "Â°C");
 		
 		farenheit = 150;
 		celsius = (farenheit - ajuste) * fator;
-		System.out.println("O resultado é " + celsius + "°C");
+		System.out.println("O resultado Ã© " + celsius + "Â°C");
 		
 		farenheit = 0;
 		celsius = (farenheit - ajuste) * fator;
-		System.out.println("O resultado é " + celsius + "°C");				
+		System.out.println("O resultado Ã© " + celsius + "Â°C");				
 	}
 }
