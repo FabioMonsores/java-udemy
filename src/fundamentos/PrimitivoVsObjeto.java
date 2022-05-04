52143afb5aca6b0897c85a5f1aa21827590ea661
Fábio Monsores <fabio2009pbi@gmail.com>
2022-05-04 18:59:39 -0300

    Tipos Primitivos vs Objetos, anotação

--- src/fundamentos/PrimitivoVsObjeto.java
+++ src/fundamentos/PrimitivoVsObjeto.java
@@ -1,5 +1,18 @@
 package fundamentos;
 
 public class PrimitivoVsObjeto {
-
+	public static void main(String[] args) {
+	
+		//String s = new String("texto");
+		
+//		No momento que vc criar um valor para uma classe, dizemos que esse valor criado é um objeto
+//		Temos os tipos primitivos em JAVA e o resto é objeto.
+		
+//		O tipo primitivo não tem a notação ponto, por ser um tipo primitivo, ele carrega apenas o valor
+//		Wrappers são a versão objeto dos tipos primitivos
+//		para cada tipo primitivos, tem um Wrapper associado
+		
+//		TUDO EM JAVA É OBJETO, MENOS OS 8 TIPOS PRIMITIVOS
+		
+	}	
 }
