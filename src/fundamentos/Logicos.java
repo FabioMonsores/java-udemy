8c838c503a3eb728aa3d5c27d310751d12b664f2
Fábio Monsores <fabio2009pbi@gmail.com>
2022-05-30 18:13:51 -0300

    tabela verdade de operadores lógicos

--- src/fundamentos/Logicos.java
+++ src/fundamentos/Logicos.java
@@ -2,7 +2,40 @@ package fundamentos;
 
 public class Logicos {
 	public static void main(String[] args) {
-		
-	}
 
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7;
+
+		System.out.println(condicao1 && condicao2);
+		System.out.println(condicao1 || condicao2);
+		System.out.println(condicao1 ^ condicao2); // ^(xor) = ou exclusivo
+		System.out.println(!condicao1);
+		System.out.println(!condicao2);
+		System.out.println(condicao1 && !condicao2);
+		System.out.println(!!condicao1); // Dupla negação, gerando assim verdadeiro
+
+//		TABELAS VERDADE
+		
+		System.out.println("\nTabela verdade E (AND)");
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+
+		System.out.println("\nTabela verdade OU (OR)");
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+
+		System.out.println("\nTabela verdade OU EXCLUSIVO (XOR)");
+		System.out.println(true  ^ true);
+		System.out.println(true  ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+		
+		System.out.println("\nTabela verdade NÃO (NOT)"); // É um operador unário
+		System.out.println(!true);
+		System.out.println(!false);
+	}
 }
